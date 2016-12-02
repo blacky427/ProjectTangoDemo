7acc088087fb47641c17a83663fbe7b39ca6d434
Blacky Chen <blacky@staging.com.tw>
2016-12-02 13:39:36 +0800

    First commit.

---
+++ Assets/Scripts/ARContrller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Tango;
+
+public class ARContrller : MonoBehaviour {
+
+	public TangoPointCloud TangoPC;
+	public Camera ARCamera;
+	public GameObject Marker;
+	public GameObject ARObject;
+	public Button PlaceBtn;
+
+	private void Awake(){
+		ARObject.SetActive(false);
+		PlaceBtn.onClick.AddListener(() => OnPlaceBtnClick());
+	}
+
+	public void OnPlaceBtnClick(){
+		ARObject.SetActive(true);
+		ARObject.transform.position = Marker.transform.position;
+		ARObject.transform.eulerAngles = Marker.transform.eulerAngles;
+	}
+
+	private void Update () {
+		SetMarker();
+	}
+
+	private void SetMarker(){
+		Vector3 centerPos = Vector3.zero;
+		Plane centerPlane = new Plane();
+		Vector2 centerScreenPos = new Vector2(Screen.width / 2f, Screen.height / 2f);
+		if(TangoPC.FindPlane(ARCamera, centerScreenPos, out centerPos, out centerPlane)){
+			Marker.transform.position = centerPos;
+			Marker.transform.up =  centerPlane.normal;
+		}
+	}
+
+}
---
+++ Assets/Scripts/KeepRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepRotation : MonoBehaviour {
+
+	public float speed = 50f;
+
+	private void Update () {
+		this.transform.localEulerAngles = new Vector3(0f, Time.time * speed, 0f);
+	}
+}
